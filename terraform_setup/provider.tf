a1b07fb77b5f8aa0f4e28919e7debee3015c9925
tawfik boujeh <tboujeh@gmail.com>
2023-09-26 11:26:55 +0200

    update dependancies for darwin_arm64.

--- terraform_setup/provider.tf
+++ terraform_setup/provider.tf
@@ -2,5 +2,14 @@ provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
   region     = "${var.aws_region}"
-  version = "~> 1.35"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
 }
